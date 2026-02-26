09b3e49a7112e1fa1b212e3f61958b27df1da044
rokadediksha <rokadediksha0@gmail.com>
2026-02-26 15:24:18 +0530

    ec2code

---
+++ ec2_instance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::487834054746:role/terraform-role"
+  }
+}
+resource "aws_instance" "first" {
+    ami="ami-0f3caa1cf4417e51b"
+    instance_type = "t3.micro"
+}
