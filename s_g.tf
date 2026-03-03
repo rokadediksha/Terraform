a6f50b0aabc5497e98b577a96e2f6926064c7a6a
rokadediksha <rokadediksha0@gmail.com>
2026-03-03 16:26:09 +0530

    sg-code

---
+++ s_g.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "ec2" {
+    ami = "ami-0f3caa1cf4417e51b"
+    instance_type = "t3.micro"
+    security_groups = [aws_security_group.ec2_sg ]
+    tags = {
+      Name="ec2"
+    }
+
+  
+}
+resource "aws_security_group" "ec2_sg" {
+    ingress {
+        description = "ssh allow"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+         }
+    egress {
+        description = "all allow"
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = [ "0.0.0.0./0" ]
+    }
+    
+  
+}
