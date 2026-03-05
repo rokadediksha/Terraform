a4af8fdfccc5a828ac2d7308ecce70ffad5c9169
Diksha Rokade <rokadediksha0@gmail.com>
2026-03-05 19:13:21 +0530

    Fix typo in security_group variable definition

--- variable.tf
+++ variable.tf
@@ -8,8 +8,8 @@ variable "instance_type" {
 variable "image" {
   default ="ami-0f3caa1cf4417e51b"
 }
-variable "swcurity_group" {
-  default = "sg-0b78b59744640c03c"
+variable "security_group" {
+  default = ["sg-0b78b59744640c03c"]
 }
 variable "key_name" {
     default = "terraform"
