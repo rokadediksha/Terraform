896ba30b3ecb7ee5a7f2bf4da026c34d07ce6c4f
Diksha Rokade <rokadediksha0@gmail.com>
2026-03-05 19:13:42 +0530

    Fix typo in security group variable name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2-instance" {
     ami =var.image
     instance_type = var.instance_type
-    vpc_security_group_ids = var.swcurity_group
+    vpc_security_group_ids = var.security_group
     key_name = var.key_name
   
 }
